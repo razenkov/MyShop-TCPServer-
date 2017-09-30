b1d3effce1eea285061d0caf34d36246095d3080
Viacheslav Razenkov <razenkov.slava@gmail.com>
2017-09-30 14:25:58 +0300

    add develop branch

--- MyShop/Program.cs
+++ MyShop/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Reflection;
 
 namespace MyShop
 {
@@ -11,54 +10,20 @@ namespace MyShop
     {
         static void Main(string[] args)
         {
-            new DllManager().ShowAllAvaliableDlls();
+            //develop
+            new MyShop().Start();
+
+
+
            
 
-            //LoadLibrarys loader = new LoadLibrarys();
-
-            //string path = @"C:\Users\adm1n\Documents\Visual Studio 2017\Projects\MyShop\MyShop";
-            //int numberOfProductsTypes = 0;
-
-            //List<string> namesOfDll = loader.GetDlls(path, ref numberOfProductsTypes);
-
-            //Console.WriteLine("-", 20);
-            //foreach (string i in namesOfDll)
-            //    Console.WriteLine(i);
-
-
-            //string st0 = namesOfDll[0];
-            //string TrimedBack = st0.TrimEnd(new char[] { '.', 'd', 'l', 'l' });
-
-            //Console.ForegroundColor = ConsoleColor.Magenta;
-            //Console.WriteLine(TrimedBack);
-            //Console.ForegroundColor = ConsoleColor.White;
-
-            //string stbuffer = "C:\\Users\\adm1n\\Documents\\Visual Studio 2017\\Projects\\MyShop\\MyShop";
-          
-            //string TrimedFront = TrimedBack.TrimStart(stbuffer.ToCharArray());
-
-            //Console.ForegroundColor = ConsoleColor.Magenta;
-            //Console.WriteLine(TrimedFront);
-            //Console.ForegroundColor = ConsoleColor.White;
-
-
-
-            //Assembly a1 = Assembly.Load(TrimedFront);
-            //Console.WriteLine("Assembly.Load(namesOfDll[1]); - DONE");
-
-            //Object o = a1.CreateInstance("vscode");
-            //Type t = a1.GetType("vscode");
-
-            //MethodInfo mi = t.GetMethod("GetName");
-            //Console.WriteLine(mi.Invoke(o, null));
 
 
 
 
-            
 
 
-            
+
 
 
 
