3d622ff3f333c6c5182f7da5e09e75efdff62603
Viacheslav Razenkov <razenkov.slava@gmail.com>
2017-09-30 13:53:51 +0300

    add DllManager

---
+++ MyShop/DllManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+
+namespace MyShop
+{
+    class DllManager
+    {
+        static List<string> dllsNames = new List<string>();
+
+        public void ShowAllAvaliableDlls()
+        {
+            string path = @"C:\Users\adm1n\Documents\Visual Studio 2017\Projects\MyShop\MyShop";
+            
+            string[] filesPathes = Directory.GetFiles(path);
+
+            List<string> dllsPathes = new List<string>();
+            //List<string> dllsNames = new List<string>();
+
+            for (int i = 0; i < filesPathes.Length; ++i)
+            {
+                if (filesPathes[i].EndsWith(".dll"))
+                    dllsPathes.Add(filesPathes[i]);
+                //File.Delete(fileEntries[i]);
+            }
+
+            dllsPathes.ForEach(Trim);
+
+            void Trim(string s)
+            {
+                string stbuffer = "C:\\Users\\adm1n\\Documents\\Visual Studio 2017\\Projects\\MyShop\\MyShop";
+                s = s.TrimEnd(new char[] { '.', 'd', 'l', 'l' });
+                s = s.TrimStart(stbuffer.ToCharArray());
+                dllsNames.Add(s);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("List of aavaliable dlls: ");
+            foreach (string i in dllsNames)
+                Console.WriteLine(i);
+            Console.ForegroundColor = ConsoleColor.White;
+
+        }
+
+        public void LoadDlls()
+        {
+            
+            //Assembly a1 = Assembly.Load(TrimedFront);
+            Console.WriteLine("Assembly.Load(namesOfDll[1]); - DONE");
+
+            //Object o = a1.CreateInstance("vscode");
+            //Type t = a1.GetType("vscode");
+
+            //MethodInfo mi = t.GetMethod("GetName");
+            //Console.WriteLine(mi.Invoke(o, null));
+        }
+    }
+}
--- MyShop/Program.cs
+++ MyShop/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
-//using Food;
 
 namespace MyShop
 {
@@ -12,120 +11,54 @@ namespace MyShop
     {
         static void Main(string[] args)
         {
-            //Assembly assembly = Assembly.Load("Flower");
+            new DllManager().ShowAllAvaliableDlls();
+           
+
+            //LoadLibrarys loader = new LoadLibrarys();
+
+            //string path = @"C:\Users\adm1n\Documents\Visual Studio 2017\Projects\MyShop\MyShop";
+            //int numberOfProductsTypes = 0;
+
+            //List<string> namesOfDll = loader.GetDlls(path, ref numberOfProductsTypes);
+
+            //Console.WriteLine("-", 20);
+            //foreach (string i in namesOfDll)
+            //    Console.WriteLine(i);
 
 
-            //Object o = assembly.CreateInstance("Flower");
+            //string st0 = namesOfDll[0];
+            //string TrimedBack = st0.TrimEnd(new char[] { '.', 'd', 'l', 'l' });
 
-            //Type t = assembly.GetType("Flower");
+            //Console.ForegroundColor = ConsoleColor.Magenta;
+            //Console.WriteLine(TrimedBack);
+            //Console.ForegroundColor = ConsoleColor.White;
 
-            //if (t == null)
-            //    Console.WriteLine("NULL");
+            //string stbuffer = "C:\\Users\\adm1n\\Documents\\Visual Studio 2017\\Projects\\MyShop\\MyShop";
+          
+            //string TrimedFront = TrimedBack.TrimStart(stbuffer.ToCharArray());
 
-
-
-
-            //Console.WriteLine(Food.Food.Foo());
-
-            LoadLibrarys loader = new LoadLibrarys();
-
-            string path = @"C:\Users\adm1n\Documents\Visual Studio 2017\Projects\MyShop\MyShop";
-            int numberOfProductsTypes = 0;
-
-            List<string> namesOfDll = loader.GetDlls(path, ref numberOfProductsTypes);
-
-            Console.WriteLine("-", 20);
-            foreach (string i in namesOfDll)
-                Console.WriteLine(i);
+            //Console.ForegroundColor = ConsoleColor.Magenta;
+            //Console.WriteLine(TrimedFront);
+            //Console.ForegroundColor = ConsoleColor.White;
 
 
-            //-----------------------------------------
-            // to add dll
 
-            string st0 = namesOfDll[0];
-            string TrimedBack = st0.TrimEnd(new char[] { '.', 'd', 'l', 'l' });
+            //Assembly a1 = Assembly.Load(TrimedFront);
+            //Console.WriteLine("Assembly.Load(namesOfDll[1]); - DONE");
 
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine(TrimedBack);
-            Console.ForegroundColor = ConsoleColor.White;
+            //Object o = a1.CreateInstance("vscode");
+            //Type t = a1.GetType("vscode");
 
-            string stbuffer = "C:\\Users\\adm1n\\Documents\\Visual Studio 2017\\Projects\\MyShop\\MyShop";
-            //int x = stbuffer.Length;
+            //MethodInfo mi = t.GetMethod("GetName");
+            //Console.WriteLine(mi.Invoke(o, null));
 
-             
+
+
+
             
 
-            string TrimedFront = TrimedBack.TrimStart(stbuffer.ToCharArray());
 
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine(TrimedFront);
-            Console.ForegroundColor = ConsoleColor.White;
-
-
-
-            Assembly a1 = Assembly.Load(TrimedFront);
-            Console.WriteLine("Assembly.Load(namesOfDll[1]); - DONE");
-
-            Object o = a1.CreateInstance("vscode");
-            Type t = a1.GetType("vscode");
-
-            MethodInfo mi = t.GetMethod("GetName");
-            Console.WriteLine(mi.Invoke(o, null));
-
-
-
-
-
-
-            //MethodInfo mi = t.GetMethod("Foo");
-
-                //mi.Invoke(o,null);
-
-                //Console.WriteLine("-", 20);
-
-
-
-
-
-            Console.ForegroundColor = ConsoleColor.White;
-            //TODO: add dlls to project and count products
-            // add data base
-
-            Base productsBase = new Base();
-            Menu menu = new Menu();
-
-
-            productsBase.InitBase();
-
-            Console.ForegroundColor = ConsoleColor.Blue;
-
-            int choice = -1;
-
-
-            while (choice != 0)
-            {
-
-                menu.InviteUser();
-                choice = Convert.ToInt32(Console.ReadLine());
-
-
-                switch (choice)
-                {
-                    case 1:
-                        productsBase.ShowBase();
-                        break;
-                    case 2:
-                        productsBase.SortByPrice();
-                        break;
-                    case 3:
-                        productsBase.SaveChanges();
-                        break;
-
-                    default:
-                        Console.WriteLine("exit");
-                        break;
-                }
-            }
+            
 
 
 
